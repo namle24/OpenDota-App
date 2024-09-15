8a774119ee63f5c0609c9a466af7b70b78267162
minhnh296 <your_email@example.com>
2024-09-15 11:32:38 +0700

    new update navigation bar

--- OpenDota/app/src/main/java/vn/edu/usth/opendota/MainActivity.java
+++ OpenDota/app/src/main/java/vn/edu/usth/opendota/MainActivity.java
@@ -1,39 +1,70 @@
 package vn.edu.usth.opendota;
-import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.tabs.TabLayout;
 
-import vn.edu.usth.opendota.player_info.player_adapter;
+import vn.edu.usth.opendota.favourite.FavouriteFragment;
+import vn.edu.usth.opendota.home.HomeFragment;
+import vn.edu.usth.opendota.search.SearchFragment;
+import vn.edu.usth.opendota.settings.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MaterialToolbar toolbar = findViewById(R.id.topappbar);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
 
-        ViewPager viewPager = findViewById(R.id.viewpager);
-        PagerAdapter adapter_1 = new player_adapter(getSupportFragmentManager());
-        viewPager.setOffscreenPageLimit(3);
-        viewPager.setAdapter(adapter_1);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                item.setChecked(true);
+                drawerLayout.closeDrawer(GravityCompat.START);
+                if (id == R.id.nav_home) {
+                    replaceFragment(new HomeFragment());
+                } else if (id == R.id.nav_favourite) {
+                    replaceFragment(new FavouriteFragment());
+                } else if (id == R.id.nav_search) {
+                    replaceFragment(new SearchFragment());
+                } else if (id == R.id.nav_settings) {
+                    replaceFragment(new SettingsFragment());
+                } else {
+                    return true;
+                }
+                return true;
+            }
+        });
 
-        TabLayout tabLayout = findViewById(R.id.tab_layout);
-        tabLayout.setupWithViewPager(viewPager);
+
     }
 
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout, fragment);
+        fragmentTransaction.commit();
+
+    }
 }
