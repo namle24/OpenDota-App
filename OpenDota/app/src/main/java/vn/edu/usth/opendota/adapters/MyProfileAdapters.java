650e39febf050a2bfe9cdc24777f99bd315d2748
minhnh296 <your_email@example.com>
2024-10-05 10:52:52 +0700

    Luu de reset project

--- OpenDota/app/src/main/java/vn/edu/usth/opendota/adapters/MyProfileAdapters.java
+++ OpenDota/app/src/main/java/vn/edu/usth/opendota/adapters/MyProfileAdapters.java
@@ -1,6 +1,5 @@
 package vn.edu.usth.opendota.adapters;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -10,7 +9,7 @@ import vn.edu.usth.opendota.ui.my_profile.MatchesFragment;
 import vn.edu.usth.opendota.ui.my_profile.OverviewFragment;
 
 public class MyProfileAdapters extends FragmentPagerAdapter {
-    private final String[] titles = new String[] { "Overview", "Matches", "Heroes" };
+    private final String[] titles = new String[]{"Overview", "Matches", "Heroes"};
 
     public MyProfileAdapters(FragmentManager fm) {
         super(fm);
@@ -20,15 +19,20 @@ public class MyProfileAdapters extends FragmentPagerAdapter {
     public int getCount() {
         return 3;
     }
+
     @Override
     public Fragment getItem(int page) {
         switch (page) {
-            case 0: return OverviewFragment.newInstance();
-            case 1: return MatchesFragment.newInstance();
-            case 2: return HeroesFragment.newInstance();
+            case 0:
+                return OverviewFragment.newInstance();
+            case 1:
+                return MatchesFragment.newInstance();
+            case 2:
+                return HeroesFragment.newInstance();
         }
         return null;
     }
+
     @Override
     public CharSequence getPageTitle(int position) {
         return titles[position];
